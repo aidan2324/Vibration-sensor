f1f9539d0904e11c535d23acf9b23efa30505f22
aidan2324 <70542787+aidan2324@users.noreply.github.com>
2022-12-05 16:04:04 -0500

    Update Vibration_sensor.py

--- Vibration_sensor.py
+++ Vibration_sensor.py
@@ -8,19 +8,27 @@ conversion_factor = 3.3/(65536)
 
 #initialize last 1/10th of a second val
 
+count = 0
 last20 = []
 for i in range(20):
         last20.append(POT_Value.read_u16() * conversion_factor)
         utime.sleep(0.01)
 
-#Main Application loop 
+#Main Application loop
 while True:
     curr = POT_Value.read_u16() * conversion_factor
     avg = sum(last20)/20
-    if curr - .01 > avg:
-        print("VOLTAGE SPIKE.  AVG:", avg, "CURR", curr)
-    #else:
-        #print(avg, curr)
-    last20.pop(0)
-    last20.append(curr)
-    utime.sleep(0.01)
+    last20.pop()
+    last20.append(POT_Value.read_u16() * conversion_factor)
+    if curr > avg * 1.5:
+        count = count + 1
+        print("Number:",  count, "VOLTAGE SPIKE.  AVG:", avg, "CURR", curr, "difference:", curr-avg, "Percent Diff:", (curr-avg)/avg*100)
+        next20 = []
+        for i in range(20):
+            next20.append(POT_Value.read_u16() * conversion_factor)
+            utime.sleep(0.01)
+        print(last20[-5:], next20[0:5])
+        for i in range(40):
+            last20.pop()
+            last20.append(POT_Value.read_u16() * conversion_factor)
+            utime.sleep(0.01)
